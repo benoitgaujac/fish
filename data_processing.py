6c3416569d14e836e9c3930bad85ac3890807278
Benoit Gaujac <benoitgaujac@eduroam-int-dhcp-97-149-158.ucl.ac.uk>
2017-03-17 13:40:18 +0000

    added extra data

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 import json
 import pdb
@@ -17,8 +18,50 @@ width = 224
 height = 224
 SEED = 66478
 
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('-d', '--im_dir', action='store', dest='im_dir',
+    help="images directory")
+
+
+def move_and_rename_data(file_dir):
+    root_dir = "../fish_results/train_data"
+    dst_dir = os.path.join(root_dir,os.path.basename(file_dir))
+    c = 0
+    for dir_name, _, file_list in os.walk(file_dir):
+        for file_name in file_list:
+            image_name = os.path.join(dir_name, file_name)
+            im = image.load_img(image_name, target_size=None)
+            dst_name = "benoit_" + file_name[:-5] + ".jpg"
+            im.save(os.path.join(dst_dir, dst_name))
+            c+=1
+            if (c+1)%200==0:
+                print("{} images processeded".format(c+1))
+
+def load_data(dir_data):
+    images = []
+    c=0
+    for root_dir, sub_dir_list, file_list in os.walk(dir_data):
+        for sub_dir in sub_dir_list:
+            sub_dir_path = os.path.join(root_dir,sub_dir)
+            for dir_name, _, file_list in os.walk(sub_dir_path):
+                for file_name in file_list:
+                    file_path = os.path.join(dir_name,file_name)
+                    image = load_image(file_path)
+                    images.append(image)
+                    c+=1
+                    if c%500==0:
+                        print("{} images loaded".format(c))
+    return images
+
+def load_image(imge_path):
+    img = image.load_img(imge_path, target_size = (width, height))
+    arr = image.img_to_array(img,data_format='channels_first').astype('float32')
+    return arr
+
 def get_mean(list_im):
-    images = np.stack([im[0] for im in list_im]).astype('float32')
+    images = np.stack([image.img_to_array(im,data_format='channels_first') for im in list_im]).astype('float32')
+    pdb.set_trace()
     mean = np.mean(images,(1,3))
     mean = np.mean(np.transpose(mean,(1,0)),(1))
     print("Mean on training set: {}, {}, {}".format(mean[0],mean[1],mean[2]))
@@ -31,6 +74,8 @@ def split_dataset(root_dir, dst_dir, val_size):
         # Remove old split
         shutil.rmtree(dst_dir)
         os.mkdir(dst_dir)
+    all_img = []
+    c=0
     for j in range(len(FISH_CLASSES)):
         images = []
         labels = []
@@ -43,11 +88,16 @@ def split_dataset(root_dir, dst_dir, val_size):
                     if im is None:
                         print("Error loading image: {}".format(image_file))
                         continue
+                    images.append([im.copy(),file_name])
+                    all_img.append(im.copy())
+                    im.close()
                     label = FISH_CLASSES[j]
-                    images.append([im,file_name])
                     labels.append(label)
                 else:
                     print("Unsupported extension: {}".format(os.path.join(dir_name, file_name)))
+            if (c+1)%500==0:
+                print("{} images loaded".format(c+1))
+            c+=1
         print("{}: read {} images.".format(FISH_CLASSES[j],len(images)))
         X_train, X_val, y_train, y_val = train_test_split(images, labels, test_size=val_size, random_state=SEED, stratify=labels)
         for i in range(len(X_train)):
@@ -79,13 +129,21 @@ def split_dataset(root_dir, dst_dir, val_size):
         print("  valditaion:\t{} images\n".format(len(X_val)))
 
 if __name__ == '__main__':
-    dataset = test_dataset("/Users/benoitgaujac/Documents/UCL/Applied ML/kaggle/Fisheries/test_stg1", 64)
+    options, arguments = parser.parse_args(sys.argv)
+    images=load_data(options.im_dir)
+    pdb.set_trace()
+    get_mean(images)
+    #split_dataset("train", "dataset", 0.15)
+    #move_and_rename_data(options.im_dir)
+
+
+    #dataset = test_dataset("/Users/benoitgaujac/Documents/UCL/Applied ML/kaggle/Fisheries/test_stg1", 64)
     #mean = get_mean(dataset.images)
     #tr = dataset.train_set
     #vl = dataset.val_set
-    batches_tr = dataset.iterate_minibatches()
-    for batch in batches_tr:
-        im, lab = batch
+    #batches_tr = dataset.iterate_minibatches()
+    #for batch in batches_tr:
+    #    im, lab = batch
 
     #batches_vl = dataset.iterate_minibatches(vl)
     #split_dataset("train", "dataset", 0.15)
